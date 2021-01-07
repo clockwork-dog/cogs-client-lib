77055d2f17a80d32c355c4d728cb6f2a7d39c9c0
Chetan Padia <chetbox@users.noreply.github.com>
2021-01-07 15:54:29 +0000

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { default as createCogsWebsocket } from './createCogsWebsocket';
+export { default as CogsClientMessage } from './types/CogsClientMessage';
+export { default as Callbacks } from './types/Callbacks';
+export { default as MediaObjectFit } from './types/MediaObjectFit';
+export { default as BackgroundOptions } from './types/BackgroundOptions';
