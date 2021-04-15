f9392aa8ef06580f2ea4a686203c635384df6e94
Chetan Padia <chetbox@users.noreply.github.com>
2021-04-15 14:59:23 +0100

    remove unused Callbacks type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as CogsConnection } from './CogsConnection';
 export { default as CogsClientMessage } from './types/CogsClientMessage';
-export { default as Callbacks } from './types/Callbacks';
+export * from './types/valueTypes';
 export { default as MediaObjectFit } from './types/MediaObjectFit';
 export { default as BackgroundOptions } from './types/BackgroundOptions';
 export { default as CogsAudioPlayer } from './AudioPlayer';
