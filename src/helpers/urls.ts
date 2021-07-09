db384a8481d75a44ef448a7257a3dad7fc767aa0
Chetan Padia <chetbox@users.noreply.github.com>
2021-07-09 09:21:09 +0100

    remove magic number

--- src/helpers/urls.ts
+++ src/helpers/urls.ts
@@ -1,4 +1,4 @@
-const COGS_SERVER_PORT = 12095;
+export const COGS_SERVER_PORT = 12095;
 
 export function assetSrc(file: string): string {
   const location = typeof window !== 'undefined' ? window.location : undefined;
