7eb440e36e09a0784cd7e42ded854bf63542790d
Chetan Padia <chetbox@users.noreply.github.com>
2022-02-11 00:32:27 +0000

    fix type error

--- src/__mocks__/dom.ts
+++ src/__mocks__/dom.ts
@@ -10,20 +10,18 @@ global.CustomEvent = dom.window.CustomEvent;
 dom.window.WebSocket = WS as any;
 global.WebSocket = dom.window.WebSocket;
 
-jest.spyOn(window.HTMLMediaElement.prototype, 'play').mockImplementation(async function () {
-  const media = this as HTMLMediaElement;
-  media.dispatchEvent(new dom.window.Event('playing', { bubbles: true }));
-  Object.defineProperty(media, 'paused', {
+jest.spyOn(window.HTMLMediaElement.prototype, 'play').mockImplementation(async function (this: HTMLMediaElement) {
+  this.dispatchEvent(new dom.window.Event('playing', { bubbles: true }));
+  Object.defineProperty(this, 'paused', {
     get() {
       return false;
     },
   });
 });
 
-jest.spyOn(window.HTMLMediaElement.prototype, 'pause').mockImplementation(async function () {
-  const media = this as HTMLMediaElement;
-  media.dispatchEvent(new dom.window.Event('paused', { bubbles: true }));
-  Object.defineProperty(media, 'paused', {
+jest.spyOn(window.HTMLMediaElement.prototype, 'pause').mockImplementation(async function (this: HTMLMediaElement) {
+  this.dispatchEvent(new dom.window.Event('paused', { bubbles: true }));
+  Object.defineProperty(this, 'paused', {
     get() {
       return true;
     },
