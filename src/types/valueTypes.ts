9bdbdbb92562cb8fe7afe35e3dc3d0e9ca38602a
Chetan Padia <chetbox@users.noreply.github.com>
2023-02-22 21:07:44 +0000

    WIP: event types, readme

--- src/types/valueTypes.ts
+++ src/types/valueTypes.ts
@@ -1,7 +1,3 @@
-export type StateValue = string | number | boolean;
-export type ConfigValue = string | number | boolean;
-export type EventValue = string | number | boolean;
-
 export enum ShowPhase {
   Setup = 'setup',
   Preshow = 'pre-show',
@@ -12,8 +8,8 @@ export enum ShowPhase {
 /*
  * Convert `{ foo: number; bar: null }` to `{ key: 'foo'; value: number } | { key: 'bar', value: undefined }`
  */
-export type EventKeyValue<TypeMap extends { [key: string]: EventValue | null }, Key extends keyof TypeMap = keyof TypeMap> = Key extends string
-  ? TypeMap[Key] extends null
+export type EventKeyValue<TypeMap extends { [key: string]: unknown }, Key extends keyof TypeMap = keyof TypeMap> = Key extends string
+  ? TypeMap[Key] extends undefined
     ? { key: Key; value: undefined }
     : { key: Key; value: TypeMap[Key] }
   : never;
