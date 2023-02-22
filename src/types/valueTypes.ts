51df71194b9940b055d882d72f259d4618844082
Chetan Padia <chetbox@users.noreply.github.com>
2023-02-22 09:26:04 +0000

    WIP: API using manifest for type inference

--- src/types/valueTypes.ts
+++ src/types/valueTypes.ts
@@ -1,4 +1,4 @@
-export type PortValue = string | number | boolean;
+export type StateValue = string | number | boolean;
 export type ConfigValue = string | number | boolean;
 export type EventValue = string | number | boolean;
 
