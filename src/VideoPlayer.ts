066be9ca30ac570f7e565c555369d0a0aa6b6f36
Chetan Padia <chetbox@users.noreply.github.com>
2024-06-25 11:55:47 +0100

    Add `store` data type with optional persisted key/values

--- src/VideoPlayer.ts
+++ src/VideoPlayer.ts
@@ -41,6 +41,7 @@ export default class VideoPlayer {
 
     // Listen for video control messages
     cogsConnection.addEventListener('message', ({ message }) => {
+      console.log('message', message);
       switch (message.type) {
         case 'media_config_update':
           this.setGlobalVolume(message.globalVolume);
